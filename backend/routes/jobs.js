49ee4ea540e3ac098381307ca024710e6caa53d9
garvitt14 <garvitthakur326@gmail.com>
2026-05-16 21:09:29 +0530

    Fix model import case sensitivity

--- backend/routes/jobs.js
+++ backend/routes/jobs.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const Job = require('../models/job')
+const Job = require('../models/Job')
 
 router.get('/', async (req, res) => {
   try {
